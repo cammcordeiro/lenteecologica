7c27e18ee4457aec3fafae08a9bedb3389968c08
18marianafernandes <18marianafernandes@gmail.com>
2026-09-24 10:39:34 -0300

    Update temp_monitoramento.R

--- data/temp_monitoramento.R
+++ data/temp_monitoramento.R
@@ -2,8 +2,10 @@
 library(tidyverse)
 
 met <- bind_rows(
-  read.csv2("~/Downloads/meteoro_mar2025-set2025.csv"),
-  read.csv2("~/Downloads/meteoro_set2024-mar2025.csv")  
+  read.csv2("~/git/lenteecologica/data/meteoro_abr2026-set2026.csv",
+  read.csv2("~/git/lenteecologica/data/meteoro_out2025-abr2026.csv"),
+  read.csv2("~/git/lenteecologica/data/meteoro_mar2025-set2025.csv"),
+  read.csv2("~/git/lenteecologica/data/meteoro_set2024-mar2025.csv")
 ) %>% 
   mutate(Data = gsub("/", "-", Data) %>% 
            as.Date(., format = "%d-%m-%Y")) %>% 
